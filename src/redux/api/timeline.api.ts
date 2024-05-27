2d6ddd0e8f1a023d6e2a32a6389257375bb5a7a7
apurbojamananis <apurbojaman.anis@gmail.com>
2024-05-27 21:18:57 +0600

    create api using RTK query

---
+++ src/redux/api/timeline.api.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const timelineApi = createApi({
+  reducerPath: "timelineApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com" }),
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => ({
+        url: "/users",
+        method: "GET",
+      }),
+    }),
+    getPosts: builder.query({
+      query: () => ({
+        url: "/comments",
+        method: "GET",
+      }),
+    }),
+    getComments: builder.query({
+      query: () => ({
+        url: "/comments",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetUsersQuery, useGetPostsQuery, useGetCommentsQuery } = timelineApi;
